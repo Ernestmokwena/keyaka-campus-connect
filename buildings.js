b86b1cf3d970604e9cfb54e4ac4a7f344e921712
Ernestmokwena <90940855+Ernestmokwena@users.noreply.github.com>
2026-05-16 19:16:24 +0200

    Create buildings.js

---
+++ buildings.js
@@ -0,0 +1,12 @@
+const campusBuildings = [
+    { lat: -23.8865878, lng: 29.7410106, name: "Maths & Computer Science Building" },
+    { lat: -23.8870926, lng: 29.7402418, name: "Digital Innovation Lab" },
+    { lat: -23.8878509, lng: 29.7409511, name: "TA Hall" },
+    { lat: -23.8874262, lng: 29.7397976, name: "Tiro Hall" },
+    { lat: -23.8882049, lng: 29.7402466, name: "School of Law" },
+    { lat: -23.8879335, lng: 29.7395395, name: "Library" },
+    { lat: -23.8885565, lng: 29.7386208, name: "S Block" },
+    { lat: -23.8868692, lng: 29.7376558, name: "SRC Chambers" },
+    { lat: -23.8877729, lng: 29.7370029, name: "Tsalas Cafe" },
+    { lat: -23.8868133, lng: 29.7363624, name: "Mashobane Res" }
+];
